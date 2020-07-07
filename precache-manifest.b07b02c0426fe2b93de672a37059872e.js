6a387f27bdc681634ef0cb9f327e855b77dfe28b
Filip Kornaus <kornaus.filip@gmail.com>
2020-07-07 19:34:46 +0200

    Updates

---
+++ precache-manifest.b07b02c0426fe2b93de672a37059872e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e14e873de6385cb07b12662254d7f645",
+    "url": "/WheaterApp-End-17.03.2020/index.html"
+  },
+  {
+    "revision": "acd8fca6e4dca5e02644",
+    "url": "/WheaterApp-End-17.03.2020/static/css/main.ce9acf00.chunk.css"
+  },
+  {
+    "revision": "455252135525e4857e24",
+    "url": "/WheaterApp-End-17.03.2020/static/js/2.08d3bc73.chunk.js"
+  },
+  {
+    "revision": "0bdb97d421f97884a5b69362f0009415",
+    "url": "/WheaterApp-End-17.03.2020/static/js/2.08d3bc73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acd8fca6e4dca5e02644",
+    "url": "/WheaterApp-End-17.03.2020/static/js/main.320238f6.chunk.js"
+  },
+  {
+    "revision": "c5a3637ca4c1e3d0579b",
+    "url": "/WheaterApp-End-17.03.2020/static/js/runtime-main.e82e31f9.js"
+  }
+]);
